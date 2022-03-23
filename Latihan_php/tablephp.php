6a1e05cfeadd2a356d42578e01e265ccae9a9cdd
Ardi <ardibbmme@gmail.com>
2022-03-23 11:46:19 +0700

    update hari ini

---
+++ Latihan_php/tablephp.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampil Data</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th> Judul</th>
+            <th> Konten </th>
+            <th> action </th>
+        </tr>
+        <?
+        //tulis array disini
+        //
+        ?>
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
+        <?php 
+        ?>
+    </table>
+</body>
+</html>
