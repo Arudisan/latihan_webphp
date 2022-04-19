af33284bb0d28b4484af673a01b28676da6da8cc
Ardi <ardibbmme@gmail.com>
2022-04-19 21:36:21 +0700

    update terbaru

--- admin/mod_menu/index.php
+++ admin/mod_menu/index.php
@@ -1,4 +1,5 @@
-<table class="table table-bordered"> 
+<button class="btn btn-primary" type="button" > Tambah Data <i class="bi bi-folder-plus "> </i></button>
+<table class="table table-bordered "> 
     <tr> 
         <th> ID Menu</th>
         <th> Nama Menu</th>
@@ -9,5 +10,54 @@
         <td> </td>
         <td> </td>
         <td> </td>
+        <td>
+            <div class="d-grid gap-1 d-md-block">
+            <button class="btn btn-primary" type="button" ><i class="bi bi-pencil-square"> </i> edit </button>
+            <button class="btn btn-primary" type="button"><i class="bi bi-trash-3"> </i> Hapus </button>
+            </div>
+      </td>
     </tr>
 </table>
+<div class="row">
+    <form action="">
+    <div class="row pt-2">
+    <div class="col-md-2"> 
+    <label for="username" class="form-label"> Nama Menu </label>
+  </div>  
+  <div class="col-md-5"> 
+    <input type="text "name="" class="form-control">  
+    </div>
+    <div class="col-md-1"></div>
+    </div>
+    <div class="row pt-2">
+    <div class="col-md-2"> 
+    <label for="username" class="form-label"> link </label>
+  </div>  
+  <div class="col-md-5"> 
+    <input type="text "name="" class="form-control">  
+    </div>
+    <div class="col-md-1"></div>
+    </div>
+    <div class="row pt-2">
+    <div class="col-md-2"> 
+    <label for="username" class="form-label"> Nama Menu </label>
+  </div>  
+  <div class="col-md-5"> 
+    <input type="text "name="" class="form-control">  
+    </div>
+    <div class="col-md-1"></div>
+    </div>
+    <div class="row pt-2">
+    <div class="col-md-2"> 
+   </div>  
+    <div class="col-md-5"> 
+    <div class="d-grid gap-2 d-md-block">
+    <button class="btn btn-secondary" type="button" ><i class="bi bi-x"> </i> Batal </button>
+    <button class="btn btn-primary" type="button"><i class="bi bi-download"> </i> Simpan </button>
+    </div>
+    <div class="col-md-1"></div>
+    </div>
+
+    </form> 
+  </div>  
+</div>
